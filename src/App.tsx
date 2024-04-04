76717da4a19083bbcf91d1dce233a2f3c1087519
Nathan <nathc@udel.edu>
2024-04-04 02:25:01 -0400

    Nathan done added his name

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload and also I'm NATHAN CHEN RAHHH.
         </p>
         <a
           className="App-link"
