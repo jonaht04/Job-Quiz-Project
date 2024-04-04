01f29155f29f2686e9414c618f4ff62df0a5d650
JuanSaq <jsaquino04@gmail.com>
2024-04-03 23:54:27 -0400

    added name to landing page

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Juan Saquino
         </p>
         <a
           className="App-link"
