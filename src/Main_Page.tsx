af0771e8db64381555cbdaae70067eea484aed38
Jonah Thomas <jonaht@udel.edu>
2024-04-09 06:36:46 -0400

    Added element to space objects in header

--- src/Main_Page.tsx
+++ src/Main_Page.tsx
@@ -4,7 +4,12 @@ import React, { useState } from 'react';
 
 function Main_Page(){
     return(
+        <div>
+            <header className="header">
+            <p>DDee Inc.</p>
 
-        
+            </header>
+        </div>
+
     )
 }
