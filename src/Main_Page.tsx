01dee13cee32681fa2ad40f8743a3bed07570aac
Jonah Thomas <jonaht@udel.edu>
2024-04-11 01:21:04 -0400

    Testing functionality for buttons navigating between pages

--- src/Main_Page.tsx
+++ src/Main_Page.tsx
@@ -4,16 +4,22 @@ import React, { useState } from 'react';
 interface Props {
     goToHomePage: () => void;
   }
+interface Props { 
+    goToBasicQuestionPage: () => void;
+}
 
-  const MainPage: React.FC<Props> = ({ goToHomePage }) => {
+interface Props { 
+    goToDetailedQuestionPage: () => void;
+}
+
     return(
         <div>
             <header className="header">
             <div className="headerBody">
-            <button>Home</button>
-            <button>Short Quiz</button>
-            <button>Long Quiz</button>
-            <button>Help</button>
+            <button className="homeButton" onClick={goToHomePage}>Home</button>
+            <button className="quizButton" onClick={goToBasicQuestionPage}>Short Questions</button>
+            <button className="saveButton">Save</button>
+            <button className="helpButton">Help</button>
             </div>
             </header>
             <h1>Welcome to the (Placeholder Name) job quiz.</h1>
