85152d66ba31d7b8c4a3efb97518935db47e6a29
Jonah Thomas <jonaht@udel.edu>
2024-04-11 01:54:02 -0400

    Made a few stylistic changes to the web page, including: Centering text in several places on the page, including buttons, via modifying the CSS file.

--- src/Main_Page.tsx
+++ src/Main_Page.tsx
@@ -17,9 +17,17 @@ interface Props {
             </div>
             </header>
             <h1>Welcome to the (Placeholder Name) job quiz.</h1>
-            <h2>There are two types of quizzes that you can take.</h2>
-            
-        </div>
+            <div>Unsure of what career is right for you? We have you covered. With our (Placeholder Name)
+                job quiz, you can discover careers perfect for you in just 10 minutes. By answering a   
+                few simple questions, we will get you on the path to success.
+            </div>
+            <h2>There are two types of quizzes that you can take:</h2>
+            <div className="promptContainer">
+                <div><button className="quizButton">Short Quiz</button>
+                <div>This is the short answer quiz. It's a decent bit shorter, but your answers will not be as in-depth.</div></div>
+                <div><button className="quizButton">Long Quiz</button>
+                <div>This is the long answer quiz. It's longer, but your answers may have greater accuracy.</div></div>
+        </div></div>
     )
 }
 export default MainPage;
