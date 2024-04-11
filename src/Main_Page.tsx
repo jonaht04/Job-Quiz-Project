fbe2ac9ed73e8f63dd98ef198a67e8ae64ccdcc8
Jonah Thomas <jonaht@udel.edu>
2024-04-11 02:52:13 -0400

    Final changes made at this point in time. Added "Spacer" class to the CSS file in order to create spread out elements on the page. Adjustments made to prior textbox classes.

--- src/Main_Page.tsx
+++ src/Main_Page.tsx
@@ -12,24 +12,33 @@ interface Props {
             <header className="header">
             <div className="headerBody">
             <button className="homeButton" onClick={goToHomePage}>Home</button>
-            <button className="saveButton">Save</button>
+            <button className="homeButton">Short Quiz</button>
+            <button className="homeButton">Long Quiz</button>
             <button className="helpButton">Help</button>
             </div>
             </header>
             <h1>Welcome to the (Placeholder Name) job quiz.</h1>
+            <p className="spacer"> </p>
             <p className="textContainer">Unsure of what career is right for you? We have you covered.</p>
             <p></p>
              <p className="textContainer">With our (Placeholder Name)
                 job quiz, you can discover careers perfect for you in just 10 minutes. By answering a   
                 few simple questions, we will get you on the path to success.
             </p>
+            
+            <p className="spacer"> </p>
+            <p className="spacer"> </p>
             <h2>There are two types of quizzes that you can take:</h2>
             <div className="promptContainer">
                 <div><button className="quizButton">Short Quiz</button>
                 <p className ="textContainer">This is the short answer quiz. It's a decent bit shorter, but your answers will not be as in-depth.</p></div>
                 <div><button className="quizButton">Long Quiz</button>
                 <p className ="textContainer">This is the long answer quiz. It consists of short answer questions, which may lead to answers of greater accuracy.</p></div>
-        </div></div>
+        </div>
+        <p></p>
+        <p></p>
+        <p className="wideTextContainer">Understand that we collect some information about you in order to suggest careers. This information is not used elsewhere for any purpose.</p>
+        </div>
     )
 }
 export default MainPage;
