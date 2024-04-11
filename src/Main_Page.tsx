c01b5bbe4a60508e5ae9baf9484f2bd7b0f454f5
Jonah Thomas <jonaht@udel.edu>
2024-04-11 01:28:06 -0400

    Added second header to further elaborate on purpose of website

--- src/Main_Page.tsx
+++ src/Main_Page.tsx
@@ -4,25 +4,21 @@ import React, { useState } from 'react';
 interface Props {
     goToHomePage: () => void;
   }
-interface Props { 
-    goToBasicQuestionPage: () => void;
-}
 
-interface Props { 
-    goToDetailedQuestionPage: () => void;
-}
 
+  const MainPage: React.FC<Props> = ({ goToHomePage }) => {
     return(
         <div>
             <header className="header">
             <div className="headerBody">
             <button className="homeButton" onClick={goToHomePage}>Home</button>
-            <button className="quizButton" onClick={goToBasicQuestionPage}>Short Questions</button>
             <button className="saveButton">Save</button>
             <button className="helpButton">Help</button>
             </div>
             </header>
             <h1>Welcome to the (Placeholder Name) job quiz.</h1>
+            <h2>There are two types of quizzes that you can take.</h2>
+            
         </div>
     )
 }
