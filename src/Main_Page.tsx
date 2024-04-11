0c0ef61fb17ca3bdd333f8634b14a6db8dc01c2b
Jonah Thomas <jonaht@udel.edu>
2024-04-11 02:36:05 -0400

    Another set of stylistic changes. Chopped up paragraphs in order to leave less blank space on the page.

--- src/Main_Page.tsx
+++ src/Main_Page.tsx
@@ -17,16 +17,18 @@ interface Props {
             </div>
             </header>
             <h1>Welcome to the (Placeholder Name) job quiz.</h1>
-            <div>Unsure of what career is right for you? We have you covered. With our (Placeholder Name)
+            <p className="textContainer">Unsure of what career is right for you? We have you covered.</p>
+            <p></p>
+             <p className="textContainer">With our (Placeholder Name)
                 job quiz, you can discover careers perfect for you in just 10 minutes. By answering a   
                 few simple questions, we will get you on the path to success.
-            </div>
+            </p>
             <h2>There are two types of quizzes that you can take:</h2>
             <div className="promptContainer">
                 <div><button className="quizButton">Short Quiz</button>
-                <div>This is the short answer quiz. It's a decent bit shorter, but your answers will not be as in-depth.</div></div>
+                <p className ="textContainer">This is the short answer quiz. It's a decent bit shorter, but your answers will not be as in-depth.</p></div>
                 <div><button className="quizButton">Long Quiz</button>
-                <div>This is the long answer quiz. It's longer, but your answers may have greater accuracy.</div></div>
+                <p className ="textContainer">This is the long answer quiz. It consists of short answer questions, which may lead to answers of greater accuracy.</p></div>
         </div></div>
     )
 }
