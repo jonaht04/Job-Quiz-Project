4a85eae1f67b763fcb0304a5e0768b996c4e08f8
JuanSaq <jsaquino04@gmail.com>
2024-04-11 14:05:02 -0400

    Linked detailed quiz button on the home page

--- src/Basic_Question1.tsx
+++ src/Basic_Question1.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import "./Basic_Question.css";
 import MainImage from './giphy.gif';
-import Button3 from './QwReiS0.gif';
-import Button4 from './xUjg9eFRCjwANWb4t4P8Qbi1QKG_w-tpxnSSwFcRaSzm2IL7QEEggDsapCWUYTX_Yo13GcLeaRDXhXfgfCUHEOaxYdX6hfR-xHfzta1js6M.gif';
+import Button3 from './old_mac.jpg'
+import Button4 from './old_mac.jpg';
 
 interface Props {
   goToHomePage: () => void;
@@ -21,14 +21,14 @@ const BasicQuestion1: React.FC<Props> = ({ goToHomePage }) => {
       </header>
 
       <h1>Question 1</h1>
-      <h2>Should Anime be allowed in UDCIScord?</h2>
+      <h2>This is a placeholder question:</h2>
       <img src={MainImage} alt="logo" />
       <div className="buttonGrid">
         <button className="bigButton">Yes</button>
         <button className="bigButton">No</button>
-        <button className="bigButton">Ban it
+        <button className="bigButton">Example with picture
           <img src={Button3} alt="logo" /></button>
-        <button className="bigButton">Legalize it
+        <button className="bigButton">Another Example w/ picture
           <img src={Button4} alt="logo" /></button>
       </div>
     </div>
