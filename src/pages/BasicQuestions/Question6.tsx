d155178851e0635f404d6a4ec2267e1412b0be45
JuanSaq <jsaquino04@gmail.com>
2024-04-18 06:00:07 -0400

    added 7 question pages that can be changed

---
+++ src/pages/BasicQuestions/Question6.tsx
@@ -0,0 +1,23 @@
+import "../Questions.css";
+import MainImage from '../../assets/giphy.gif';
+import Button3 from '../../assets/old_mac.jpg'
+import Button4 from '../../assets/old_mac.jpg';
+
+export function Question6() {
+    return (<div className='container'>
+    <h1>Question 6</h1>
+    <h2>This is a placeholder question:</h2>
+    <img className="MainImage" src={MainImage} alt="logo" />
+    <div className="buttonGrid">
+      <button className="bigButton">Yes</button>
+      <button className="bigButton">No</button>
+      <button className="bigButton">Example with picture
+      <div></div>
+        <img src={Button3} alt="logo" /></button>
+      <button className="bigButton">Another Example w/ picture
+        <img src={Button4} alt="logo" /></button>
+    </div>
+  </div>
+)
+}
+export default Question6;
--- src/pages/Basic_Question1.tsx
+++ src/pages/Basic_Question1.tsx
@@ -2,6 +2,12 @@ import React, { useState} from 'react';
 import "./Questions.css";
 import SettingsMenu from '../Setting_menu';
 import Question1 from './BasicQuestions/Question1';
+import Question2 from './BasicQuestions/Question2';
+import Question3 from './BasicQuestions/Question3';
+import Question4 from './BasicQuestions/Question4';
+import Question5 from './BasicQuestions/Question5';
+import Question6 from './BasicQuestions/Question6';
+import Question7 from './BasicQuestions/Question7';
 
 interface Props {
   goToHomePage: () => void;
@@ -55,16 +61,19 @@ const BasicQuestion1: React.FC<Props> = ({ goToHomePage , isDarkMode, toggleDark
       </header>
 
       <div className='container'>
-        {currentPage === 1 ? <Question1/> : <p>page 2</p>}
-
+        {currentPage === 1 ?
+         <Question1/> : currentPage === 2 ?
+          <Question2/> : currentPage === 3 ?
+           <Question3/> : currentPage === 4 ?
+            <Question4/> : currentPage === 5 ?
+             <Question5/> : currentPage === 6 ?
+              <Question6/> : <Question7/>}
       </div>
       <div className='container'>
-        <button className="changeProgressButton" onClick={() => {
-          if (currentPage !== 1) setCurrentPage(currentPage - 1);
-        }}>Previous Question</button>
-        <button className="changeProgressButton" onClick={() => {
-          if (currentPage !== 7) setCurrentPage(currentPage + 1);
-        }}>Next Question</button>
+        <button className="changeProgressButton" disabled={currentPage===1} onClick={() => {
+          setCurrentPage(currentPage - 1)}}>Previous Question</button>
+        <button className="changeProgressButton" disabled={currentPage===7} onClick={() => {
+          setCurrentPage(currentPage + 1);}}>Next Question</button>
       </div>
     </div>
   );
