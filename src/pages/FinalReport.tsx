2cab6d1de5a006edb701bf744545d756c2b1a472
Nathan <nathc@udel.edu>
2024-04-23 17:03:52 -0400

    added final report implementation at end of detailed questions

--- src/pages/FinalReport.tsx
+++ src/pages/FinalReport.tsx
@@ -21,6 +21,13 @@ interface Props {
       };
       return (
         <div>
+            {/*Header Text*/}
+            <h1>General Field: INSERT VARIABLE HERE</h1>
+            <div className="finalReportSummary">Summary: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque hendrerit eget mi sit amet laoreet. Nam fringilla est a fringilla egestas. Donec vitae fermentum odio, a imperdiet tortor. Phasellus sed quam dui. Curabitur tortor dui, pulvinar sed auctor eget, hendrerit eget ex. Pellentesque maximus ipsum id lacinia convallis. Pellentesque aliquam eu velit in placerat. Morbi dapibus nibh mi, quis tempor magna cursus sit amet. </div>
+            
+            <div className="finalReportSubheader">Attributes of User:</div>
+            <div className="finalReportSubheader">Possible Jobs:</div>
+
             {/*Settings Menu*/}
               <SettingsMenu isOpen={isSettingsOpen} onClose={toggleSettings} onDarkModeToggle={toggleDarkMode} isDarkMode={isDarkMode}/>
               <div className={`dropdown ${isDropdownOpen ? 'open' : ''}`}>
