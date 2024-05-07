40995321bb067428a62e485e097dee5a74eeb0fa
Alexander-Lee390 <alexblee@udel.edu>
2024-05-07 15:48:34 -0400

    fixing load system

--- src/pages/BasicQuestions/Business Questions/BusinessQ1.tsx
+++ src/pages/BasicQuestions/Business Questions/BusinessQ1.tsx
@@ -1,4 +1,8 @@
 import "../../Questions.css";
+import Marketing from '../../../assets/SuiseiStonks.png'
+import Finance from '../../../assets/FInance.jpeg'
+import Management from '../../../assets/managment.webp'
+import Entrepreneurship from '../../../assets/Entrepreneurship.png'
 
 interface Props {
   setSelectedString: (selectedString: string) => void;
@@ -16,10 +20,14 @@ export const BusinessQ1: React.FC<Props> = ({ setSelectedString, handleAnswerSel
       <h1><strong>Business Branch: Question 1</strong></h1>
       <h2>Which area of business are you most interested in?</h2>
       <div className="answerGrid">
-        <button className="answerButton" onClick={() => handleButtonClick("Finance")}>Finance</button>
-        <button className="answerButton" onClick={() => handleButtonClick("Marketing")}>Marketing</button>
-        <button className="answerButton" onClick={() => handleButtonClick("Management")}>Management</button>
-        <button className="answerButton" onClick={() => handleButtonClick("Entrepreneurship")}>Entrepreneurship</button>
+        <button className="answerButton" onClick={() => handleButtonClick("Finance")}>Finance
+        <img src={Finance} alt="logo" style={{ height: "250px", width: "400px"}}></img></button>
+        <button className="answerButton" onClick={() => handleButtonClick("Marketing")}>Marketing
+        <img src={Marketing} alt="logo" style={{ height: "250px", width: "400px"}}></img></button>
+        <button className="answerButton" onClick={() => handleButtonClick("Management")}>Management
+        <img src={Management} alt="logo" style={{ height: "250px", width: "400px"}}></img></button>
+        <button className="answerButton" onClick={() => handleButtonClick("Entrepreneurship")}>Entrepreneurship
+        <img className="image" src={Entrepreneurship} alt="logo" style={{ height: "250px", width: "400px"}}></img></button>
       </div>
       <div className="spacer"></div>
       <button className="answerButton" onClick={() => handleButtonClick("Consulting")}>Consulting</button>
