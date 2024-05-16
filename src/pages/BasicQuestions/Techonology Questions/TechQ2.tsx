74e8c8f0b4d4b1225ba403d4c9388b6ef0bc29de
JuanSaq <jsaquino04@gmail.com>
2024-05-16 01:15:18 -0400

    Made buttons in Technology Branch clear when pressed

--- src/pages/BasicQuestions/Techonology Questions/TechQ2.tsx
+++ src/pages/BasicQuestions/Techonology Questions/TechQ2.tsx
@@ -1,3 +1,4 @@
+import React, {useState} from "react";
 import "../../Questions.css";
 
 interface Props {
@@ -5,8 +6,13 @@ interface Props {
 }
 
 export const TechQ2: React.FC<Props> = ({handleAnswerSelect }) => {
+  const [buttonSelected, setButtonSelected] = useState(0);
+
   const handleButtonClick = (selectedString: string) => {
     handleAnswerSelect(selectedString);
+    if (selectedString === "Hardware") setButtonSelected(1);
+    else if (selectedString === "Software") setButtonSelected(2);
+    else if (selectedString === "Hardware & Software") setButtonSelected(3);
   };
 
   return (
@@ -14,11 +20,11 @@ export const TechQ2: React.FC<Props> = ({handleAnswerSelect }) => {
       <h1><strong>Technology Branch: Question 2</strong></h1>
       <h2>Are you interested in working with hardware or software development?</h2>
       <div className="answerGrid">
-        <button className="answerButton" style={{backgroundColor: "#a6e3a1"}} onClick={() => handleButtonClick("Hardware")}>Hardware</button>
-        <button className="answerButton" style={{backgroundColor: "#cba6f7"}} onClick={() => handleButtonClick("Software")}>Software</button>
+        <button className="answerButton" disabled={buttonSelected===1} style={{backgroundColor: "#a6e3a1"}} onClick={() => handleButtonClick("Hardware")}>Hardware</button>
+        <button className="answerButton" disabled={buttonSelected===2} style={{backgroundColor: "#cba6f7"}} onClick={() => handleButtonClick("Software")}>Software</button>
       </div>
       <div className="spacer"></div>
-      <button className="answerButton" style={{backgroundColor: "#74c7ec"}} onClick={() => handleButtonClick("Hardware & Software")}>Both</button>
+      <button className="answerButton" disabled={buttonSelected===3} style={{backgroundColor: "#74c7ec"}} onClick={() => handleButtonClick("Hardware & Software")}>Both</button>
     </div>
   )
 }
--- src/pages/BasicQuestions/Techonology Questions/TechQ6.tsx
+++ src/pages/BasicQuestions/Techonology Questions/TechQ6.tsx
@@ -1,3 +1,4 @@
+import React, {useState} from "react";
 import "../../Questions.css";
 
 interface Props {
@@ -5,8 +6,16 @@ interface Props {
 }
 
 export const TechQ6: React.FC<Props> = ({handleAnswerSelect }) => {
+  const [buttonSelected, setButtonSelected] = useState(0);
+
   const handleButtonClick = (selectedString: string) => {
     handleAnswerSelect(selectedString);
+    if (selectedString === "Healthcare") setButtonSelected(1);
+    else if (selectedString === "Finance") setButtonSelected(2);
+    else if (selectedString === "Entertainment") setButtonSelected(3);
+    else if (selectedString === "Education") setButtonSelected(4);
+    else if (selectedString === "Robotics/AI") setButtonSelected(5);
+    else if (selectedString === "Transportation") setButtonSelected(6);
   };
 
   return (
@@ -14,12 +23,12 @@ export const TechQ6: React.FC<Props> = ({handleAnswerSelect }) => {
       <h1><strong>Technology Branch: Question 6</strong></h1>
       <h2>What industries are you interested in applying your technology skills to?</h2>
       <div className="answerGrid">
-        <button className="answerButton" style={{backgroundColor: "#a6e3a1"}} onClick={() => handleButtonClick("Healthcare")}>Healthcare</button>
-        <button className="answerButton" style={{backgroundColor: "#cba6f7"}} onClick={() => handleButtonClick("Finance")}>Finance</button>
-        <button className="answerButton" style={{backgroundColor: "#74c7ec"}} onClick={() => handleButtonClick("Entertainment")}>Entertainment</button>
-        <button className="answerButton" style={{backgroundColor: "#f38ba8"}} onClick={() => handleButtonClick("Education")}>Education</button>
-        <button className="answerButton" style={{backgroundColor: "#f9e2af"}} onClick={() => handleButtonClick("Robotics/AI")}>Robotics/AI</button>
-        <button className="answerButton" style={{backgroundColor: "#fab387"}}onClick={() => handleButtonClick("Transportation")}>Transportation</button>
+        <button className="answerButton" disabled={buttonSelected===1} style={{backgroundColor: "#a6e3a1"}} onClick={() => handleButtonClick("Healthcare")}>Healthcare</button>
+        <button className="answerButton" disabled={buttonSelected===2} style={{backgroundColor: "#cba6f7"}} onClick={() => handleButtonClick("Finance")}>Finance</button>
+        <button className="answerButton" disabled={buttonSelected===3} style={{backgroundColor: "#74c7ec"}} onClick={() => handleButtonClick("Entertainment")}>Entertainment</button>
+        <button className="answerButton" disabled={buttonSelected===4} style={{backgroundColor: "#f38ba8"}} onClick={() => handleButtonClick("Education")}>Education</button>
+        <button className="answerButton" disabled={buttonSelected===5} style={{backgroundColor: "#f9e2af"}} onClick={() => handleButtonClick("Robotics/AI")}>Robotics/AI</button>
+        <button className="answerButton" disabled={buttonSelected===6} style={{backgroundColor: "#fab387"}}onClick={() => handleButtonClick("Transportation")}>Transportation</button>
       </div>
     </div>
   )
