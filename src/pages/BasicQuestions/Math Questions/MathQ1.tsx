0d0d1fef2880a4fc4b70469b0a46a32b074d5937
JuanSaq <jsaquino04@gmail.com>
2024-05-16 01:05:17 -0400

    Made buttons in Math branch clear when pressed

--- src/pages/BasicQuestions/Math Questions/MathQ1.tsx
+++ src/pages/BasicQuestions/Math Questions/MathQ1.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import "../../Questions.css";
 
 interface Props {
@@ -5,8 +6,13 @@ interface Props {
 }
 
 export const MathQ1: React.FC<Props> = ({handleAnswerSelect }) => {
+  const [buttonSelected, setButtonSelected] = useState(0);
+
   const handleButtonClick = (selectedString: string) => {
     handleAnswerSelect(selectedString);
+    if (selectedString === "Pure Mathematics") setButtonSelected(1);
+    else if (selectedString === "Applied Mathematics") setButtonSelected(2);
+    else if (selectedString === "Statistics") setButtonSelected(3);
   };
 
   return (
@@ -14,11 +20,11 @@ export const MathQ1: React.FC<Props> = ({handleAnswerSelect }) => {
       <h1><strong>Mathematics Branch: Question 1</strong></h1>
       <h2>What specific area of mathematics are you most passionate about?</h2>
       <div className="answerGrid">
-        <button className="answerButton" style={{backgroundColor: "#a6e3a1"}} onClick={() => handleButtonClick("Software Engineering")}>Pure mathematics</button>
-        <button className="answerButton" style={{backgroundColor: "#cba6f7"}} onClick={() => handleButtonClick("Software Engineering")}>Applied mathematics</button>
+        <button className="answerButton" disabled={buttonSelected===1} style={{backgroundColor: "#a6e3a1"}} onClick={() => handleButtonClick("Pure Mathematics")}>Pure Mathematics</button>
+        <button className="answerButton" disabled={buttonSelected===2} style={{backgroundColor: "#cba6f7"}} onClick={() => handleButtonClick("Applied Mathematics")}>Applied Mathematics</button>
       </div>
       <div className="spacer"></div>
-      <button className="answerButton" style={{backgroundColor: "#74c7ec"}} onClick={() => handleButtonClick("Software Engineering")}>Statistics</button>
+      <button className="answerButton" disabled={buttonSelected===3} style={{backgroundColor: "#74c7ec"}} onClick={() => handleButtonClick("Statistics")}>Statistics</button>
     </div>
   )
 }
--- src/pages/BasicQuestions/Math Questions/MathQ3.tsx
+++ src/pages/BasicQuestions/Math Questions/MathQ3.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import "../../Questions.css";
 
 interface Props {
@@ -5,8 +6,13 @@ interface Props {
 }
 
 export const MathQ3: React.FC<Props> = ({handleAnswerSelect }) => {
+  const [buttonSelected, setButtonSelected] = useState(0);
+
   const handleButtonClick = (selectedString: string) => {
     handleAnswerSelect(selectedString);
+    if (selectedString === "Yes") setButtonSelected(1);
+    else if (selectedString === "No") setButtonSelected(2);
+    else if (selectedString === "Somewhat") setButtonSelected(3);
   };
 
   return (
@@ -14,11 +20,11 @@ export const MathQ3: React.FC<Props> = ({handleAnswerSelect }) => {
       <h1><strong>Mathematics Branch: Question 3</strong></h1>
       <h2>Do you have experience or interest in using mathematical modeling and simulation techniques?</h2>
       <div className="answerGrid">
-        <button className="answerButton" style={{backgroundColor: "#a6e3a1"}} onClick={() => handleButtonClick("Yes")}>Yes</button>
-        <button className="answerButton" style={{backgroundColor: "#cba6f7"}} onClick={() => handleButtonClick("No")}>No</button>
+        <button className="answerButton" disabled={buttonSelected===1} style={{backgroundColor: "#a6e3a1"}} onClick={() => handleButtonClick("Yes")}>Yes</button>
+        <button className="answerButton" disabled={buttonSelected===2} style={{backgroundColor: "#cba6f7"}} onClick={() => handleButtonClick("No")}>No</button>
       </div>
       <div className="spacer"></div>
-      <button className="answerButton" style={{backgroundColor: "#74c7ec"}} onClick={() => handleButtonClick("Somewhat")}>Somewhat</button>
+      <button className="answerButton" disabled={buttonSelected===3} style={{backgroundColor: "#74c7ec"}} onClick={() => handleButtonClick("Somewhat")}>Somewhat</button>
     </div>
   )
 }
